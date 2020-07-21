903addefd86d17b3e31987baf7f78d0775f663da
Bill <bglesias@gmail.com>
2020-07-20 23:09:27 -0400

    add reproduction of vue test utils immediate watch issue

--- src/components/TestComponent.spec.js
+++ src/components/TestComponent.spec.js
@@ -1,8 +1,25 @@
-import TestComponent from './TestComponent'
-import { shallowMount } from '@vue/test-utils'
+import TestComponent from "./TestComponent";
+import { shallowMount } from "@vue/test-utils";
 
-describe('test issue', () => {
-    it('reproduces issue', () => {
-        const wrapper = shallowMount(TestComponent)
+describe("test issue", () => {
+  it("reproduces issue", async () => {
+    const wrapper = shallowMount(TestComponent, {
+      propsData: {
+        myProp: "prop1",
+      },
+    });
+
+    wrapper.setProps({
+      myProp: "prop2"
     })
-})
+
+    // this works
+    expect(wrapper.vm.myProp).toEqual('prop2')
+
+    // but after next tick runs
+    await wrapper.vm.$nextTick()
+
+    // the prop is prop1, where it should be prop2
+    expect(wrapper.vm.myProp).toEqual('prop2')
+  });
+});
--- src/components/TestComponent.vue
+++ src/components/TestComponent.vue
@@ -7,8 +7,27 @@
 <script>
 
 export default {
+  props: {
+    myProp: {
+      type: String
+    }
+  },
   data: () => ({
     msg: null
-  })
+  }),
+  computed: {
+    myPropComputed() {
+      return this.myProp
+    }
+  },
+  watch: {
+    myPropComputed: {
+      // comment out 'immediate: true' to pass test
+      immediate: true,
+      handler() {
+        this.msg = this.myPropComputed
+      }
+    }
+  }
 }
 </script>
